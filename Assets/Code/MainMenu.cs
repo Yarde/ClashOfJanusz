0377ae85054c07060787c1555bfa7bf3e61354f5
Dawid Kaluża <dawid.kaluza@tensquaregames.com>
2021-04-23 23:41:41 +0200

    main menu and loading screen

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button play;
+    public Button settings;
+    public Button exit;
+    public UI.Settings Settings;
+    
+    void Start()
+    {
+        play.onClick.AddListener(PlayGame);
+        settings.onClick.AddListener(OpenSettings);
+        exit.onClick.AddListener(ExitGame);
+    }
+    
+    private void PlayGame()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+    
+    private void OpenSettings()
+    {
+        Settings.gameObject.SetActive(true);
+    }
+    
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
